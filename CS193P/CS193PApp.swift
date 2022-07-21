f0047a4817f86a3efa7a1cd3e0396ea99608b5fd
SIE-KYIN <kjsl4tw@naver.com>
2022-07-21 13:49:59 +0900

    first

---
+++ CS193P/CS193PApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193PApp.swift
+//  CS193P
+//
+//  Created by Kyu jin Lee on 2022/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CS193PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
